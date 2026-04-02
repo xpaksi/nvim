5828c78e91f83710cdf1b6c63a499bc508ea257a
Haekal Kalipaksi <hkalipaksi@proton.me>
2026-04-02 10:05:36 +0700

    feat: use nvim tree

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -3,7 +3,26 @@ vim.pack.add({
 	{ src = "https://github.com/nvim-tree/nvim-tree.lua" },
 })
 
+local api = require("nvim-tree.api")
+
+local function toggle_replace()
+	if api.tree.is_visible() then
+		api.tree.close()
+	else
+		api.tree.open({ current_window = true })
+	end
+end
+
+local function on_attach(bufnr)
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
+
+	vim.keymap.set("n", "<CR>", api.node.open.replace_tree_buffer, opts("Open: In Place"))
+end
+
 require("nvim-tree").setup({
+	on_attach = on_attach,
 	filters = {
 		dotfiles = true,
 	},
@@ -18,9 +37,9 @@ require("nvim-tree").setup({
 	},
 })
 
-vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeFocus<cr>", {
+vim.keymap.set("n", "<leader>e", toggle_replace, {
 	silent = true,
-	desc = "Toggle NvimTree",
+	desc = "Toggle NvimTree (Vinegar style)",
 })
 
 vim.keymap.set("n", "<leader>E", "<cmd>NvimTreeToggle<cr>", {
